a2cb522044ed47edaf2f471fa430c04fa9256e3e
igormassami9 <igormassami9@gmail.com>
2023-07-15 23:02:18 -0300

    first commit

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+    <?php 
+        // 0x = Hexadecimal 
+        // 0b = Binário
+        // 0 = Octal
+        // $num = 010;
+        // echo "O valor da variável é $num";
+
+        // Diz o tipo de primitivo (float, double, string, etc..)
+        // $v = "Igor";
+        // var_dump($v);
+    ?>
+</body>
+</html>
---
+++ teste/index.php
@@ -0,0 +1,14 @@
+<?php 
+// $nome = "Igor";
+// $sobrenome = "Massami";
+// $peso = 55.03;
+// $idade = 22;
+// $casado = false;
+// const CURSO = "TI";
+
+// const PAIS = "Brasil";
+
+// echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+
+
+?> 
